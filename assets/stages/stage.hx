8a409131c46cf195210b7d32ff103d2c9f907b0f
Fllicss <156634467+Fllicss@users.noreply.github.com>
2024-10-15 19:20:05 -0300

    week 1 stage fixes + use of addBG

--- assets/stages/stage.hx
+++ assets/stages/stage.hx
@@ -1,13 +1,13 @@
-function create() {
+function createPost() {
 	var stageBack = new FunkinSprite().loadTexture('stageback');
 	stageBack.setPosition(-1200, -200);
 	stageBack.scrollFactor.set(.9, .9);
-	add(stageBack);
+	addBG(stageBack);
 
 	var stageFront = new FunkinSprite().loadTexture('stagefront');
 	stageFront.setPosition(-1250, 550);
 	stageFront.scrollFactor.set(.9, .9);
-	add(stageFront);
+	addBG(stageFront);
 
 	var stageCurtains = new FunkinSprite().loadTexture('stagecurtains');
 	stageCurtains.setPosition(-1250, -300);
@@ -15,15 +15,4 @@ function create() {
 	stageCurtains.scrollFactor.set(1.3, 1.3);
 	stageCurtains.updateHitbox();
 	add(stageCurtains);
-}
-
-function createPost() {
-	var bf = state.player1;
-	var dad = state.player2;
-	var gf = state.player3;
-
-	/*bf.setPosition(50,350);
-	dad.setPosition(-600,0);
-	gf.setPosition(-323,0);
-	state.defaultCamZoom = state.camGame.zoom = 0.9;*/
 }
