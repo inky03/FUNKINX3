9ab995b45101ec8fccd77f2bb8760e68750bc192
ashley <84390581+inky03@users.noreply.github.com>
2025-05-27 05:29:23 -0600

    Update TitleState.hx
    fix formatting and better color fade

--- source/funkin/states/TitleState.hx
+++ source/funkin/states/TitleState.hx
@@ -13,6 +13,9 @@ class TitleState extends FunkinState {
 	public var enter:FunkinSprite;
 	public var logo:FunkinSprite;
 	
+	public var enterColors:Array<FlxColor> = [0xff33ffff, 0xff3333cc];
+	var enterColorIndex:Int = 0;
+	
 	public var introTexts:Array<Array<String>> = [];
 	public var currentIntroText:Array<String>;
 	public var titleStarted:Bool = false;
@@ -67,9 +70,9 @@ class TitleState extends FunkinState {
 		titleGroup.add(enter);
 		
 		ngSpr = new FunkinSprite();
-	    if (FlxG.random.bool(1)) {
+		if (FlxG.random.bool(1)) {
 			ngSpr.loadGraphic(Paths.image('titlescreen/ngClassic'));
-	    } else if (FlxG.random.bool(30)) {
+		} else if (FlxG.random.bool(30)) {
 			ngSpr.loadGraphic(Paths.image('titlescreen/ngAnimated'), true, 600);
 			ngSpr.setGraphicSize(Std.int(ngSpr.width * 0.55));
 			ngSpr.addAnimation('idle', null, 4, true, [0, 1]);
@@ -81,15 +84,16 @@ class TitleState extends FunkinState {
 		}
 		ngSpr.alpha = .0001;
 		ngSpr.updateHitbox();
-    	ngSpr.screenCenter(X);
-    	ngSpr.y = FlxG.height - ngSpr.height - 160;
+		ngSpr.screenCenter(X);
+		ngSpr.y = FlxG.height - ngSpr.height - 160;
 		
 		playMusic(MainMenuState.menuMusic, !skipIntro);
 		conductorInUse.syncTracker = FlxG.sound.music;
+		conductorInUse.sync();
 		add(introGroup);
 		
 		if (!skipIntro) {
-    		add(ngSpr);
+			add(ngSpr);
 			final ySpacing:Float = 60;
 			
 			queueEvent(beatToMS(1), (_) -> {
@@ -126,6 +130,8 @@ class TitleState extends FunkinState {
 		
 		DiscordRPC.presence.details = 'In the title screen';
 		DiscordRPC.dirty = true;
+		
+		enter.color = enterColors[enterColorIndex ++];
 	}
 	public override function update(elapsed:Float) {
 		super.update(elapsed);
@@ -154,10 +160,12 @@ class TitleState extends FunkinState {
 		logo.playAnimation('bump', true);
 		if (beat % 2 == 0) {
 			if (!confirmed) {
-				var to:FlxColor = 0xff3333cc;
-				var from:FlxColor = 0xff33ffff;
+				if (enterColorIndex >= enterColors.length) enterColorIndex = 0;
+				
+				var to:FlxColor = enterColors[enterColorIndex ++];
+				
 				FlxTween.cancelTweensOf(enter);
-				FlxTween.color(enter, conductorInUse.crochet * .001 * 2, from, to, {type: (beat % 4 < 2 ? ONESHOT : BACKWARD)});
+				FlxTween.color(enter, conductorInUse.crochet * .001 * 2, enter.color, to);
 			}
 		}
 	}
